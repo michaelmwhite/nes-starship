dd1a7c52387e1b399e8a2b21631cc5949831dc4c
michaelmwhite <michael.m.white@live.com>
2021-01-30 12:08:00 -0500

    updated background and attributes

--- graphics/palettes.asm
+++ graphics/palettes.asm
@@ -1,2 +1,2 @@
-  .db $0F,$21,$15,$30, $0F,$0F,$0F,$0F, $0F,$0F,$0F,$0F, $0F,$0F,$0F,$0F  ; background palettes - 4 different color schemes
+  .db $0F,$20,$25,$2C, $0F,$0F,$0F,$0F, $0F,$0F,$0F,$0F, $0F,$0F,$0F,$0F  ; background palettes - 4 different color schemes
   .db $0F,$1c,$2b,$39, $0F,$0F,$0F,$0F, $0F,$0F,$0F,$0F, $0F,$0F,$0F,$0F  ; sprite palettes
