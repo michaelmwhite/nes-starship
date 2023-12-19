f97204cbc38f4a2a355458eb6402a3c9dc2c2f3c
Joshua Kim <20001595+joshua-kim@users.noreply.github.com>
2023-12-19 15:40:14 -0500

    remove bugs
    Signed-off-by: Joshua Kim <20001595+joshua-kim@users.noreply.github.com>

--- graphics/sprites.asm
+++
@@ -1,4 +0,0 @@
-    .db $78, $04, $00, $80      ; each sprite is represented by 4 bytes - 1 byte each for their y position in pixels, the tile id in the 
-                                ; sprite sheet, sprite attributes such as color palette, and x position in pixels
-                                ; making a separate file for this as many main character sprites contain multiple tiles and it's an 
-                                ; easy way to consolidate that info, although here I use a single tile for the mc
